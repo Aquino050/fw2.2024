3e21a892a8476b9a9093a495d31400ccda6130ab
Aquino050 <vitoria.aquino+github@estudante.ifms.edu.br>
2024-09-30 14:12:47 +0000

    Aula 30/09

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,23 @@
 import Footer from "./modulos/footer";
 import Navbar from "./modulos/navbar";
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 export default function Index() {
   return<div>
     <Navbar />
     Bem Vindos
-    <Footer />
-  </div>    
+    <Container>
+      <Row>
+        <Col className="bg-danger">1 of 2</Col>
+        <Col className="bg-primary">2 of 2</Col>
+      </Row>
+      <Row>
+        <Col className="bg-success">1 of 3</Col>
+        <Col className="bg-warning">2 of 3</Col>
+        <Col className="bg-info">3 of 3</Col>
+      </Row>
+    </Container>
+    <Footer /> 
+  </div>      
 }
